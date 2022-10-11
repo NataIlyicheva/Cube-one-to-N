6a5e62297476adea34e18b1fa91eaaef8969184d
Natali <n7483205@ya.ru>
2022-10-11 21:23:55 +0700

    extra lines cleared

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
-﻿/* программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-3 -> 1, 8, 27
-5 -> 1, 8, 27, 64, 125 */
-
-void GetNumber (int Number)
+﻿void GetNumber (int Number)
 {
     Console.WriteLine($" {Number} - ");
     for (int i = 1; i <= Number; i++)
@@ -16,4 +12,3 @@ void GetNumber (int Number)
 Console.WriteLine("Введите число: ");
 int Number = int.Parse(Console.ReadLine()!);
 GetNumber(Number);
-
